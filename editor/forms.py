407376a5b7b97d0761d2b90cbc07b52ac9c817e2
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-03-02 20:10:34 +0700

    Tạo Form model cho bài viết

--- editor/forms.py
+++ editor/forms.py
@@ -2,6 +2,8 @@ from typing import Any
 from django import forms
 from django.contrib.auth import authenticate
 
+from .models import Post
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100, required=True)
@@ -18,3 +20,17 @@ class RegisterForm(forms.Form):
         if self.cleaned_data['password'] != self.cleaned_data['c_password']:
             raise forms.ValidationError("Passwords don't match")
         return super().clean()
+    
+# Post section
+    
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ['title', 
+                  'slug',
+                  'description', 
+                  'content',
+                  'keyword',
+                  'pub_date']
+        exclude = ['author']
