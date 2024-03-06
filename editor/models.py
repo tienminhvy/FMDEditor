5021de765b7a92cb8e8dbe41ffa0991abcb567f1
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-03-06 11:03:54 +0700

    Updated model related name for Post - Comment

--- editor/models.py
+++ editor/models.py
@@ -27,4 +27,4 @@ class Comment(models.Model):
     created_date = models.DateTimeField(verbose_name="created date", auto_now_add=True)
     updated_date = models.DateTimeField(verbose_name="updated date", auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
